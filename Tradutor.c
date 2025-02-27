fb765edb39036055eb1fdb6918eda78ed88adac3
Danrlei Almeida Araujo <danrleiaraujo@gmail.com>
2025-02-26 23:46:20 -0300

    Organização do código

--- Tradutor.c
+++ Tradutor.c
@@ -9,31 +9,31 @@
 
 #include "pico/bootrom.h" // Para o bootload
 
+// Comunicacao I2C para Display ssd1306
 #define I2C_PORT i2c1
 #define I2C_SDA 14
 #define I2C_SCL 15
 #define endereco 0x3C
 
 // Led RGB
-const uint LED_G = 11;
-const uint LED_B = 12;
-const uint LED_R = 13;
+#define LED_G 11
+#define LED_B 12
+#define LED_R 13
 
-#define BUZZER_PIN 21
+#define BUZZER 21 // GPIO do Buzzer
 
-#define botao_A 5 // GPIO para botão A
-#define botao_B 6 // GPIO para botão B
-#define botao_Joystick 22 // GPIO para botão do Joystick
-
+#define BOTAO_A 5 // GPIO para botão A
+#define BOTAO_B 6 // GPIO para botão B
+#define BOTAO_JOYSTICK 22 // GPIO para botão do Joystick
 
 #define DEBOUNCE_TIME 200000 // 200ms em us de debounce
 uint32_t last_time = 0;      // captura o tempo do ultimo acionamento do botão para o debounce
 
-bool select = false;
+bool select = false; // Variavel que ativa quando seleciona a letra
 
-int letra = 0;
+int letra = 0; // Contador para percorrer o alfabeto
 const char alfabeto[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const char morse[28][5] = {
+const char morse[28][5] = { // Representacao do alfabeto em morse
   ".-",   // A
   "-...", // B
   "-.-.", // C
@@ -62,77 +62,36 @@ const char morse[28][5] = {
   "--.."  // Z
 };
 
-// Função alternativa para gerar som SEM PWM (Buzzer Toggling)
-void buzzerBeep(uint duration) {
-  for (int i = 0; i < duration; i++) {
-      gpio_put(BUZZER_PIN, 1);
-      sleep_ms(5);
-      gpio_put(BUZZER_PIN, 0);
-      sleep_ms(5);
-  }
-}
-
-void traco(){
-  gpio_put(LED_R, 1); 
-  buzzerBeep(80);
-  gpio_put(LED_R, 0);
-}
-
-void ponto(){  
-  gpio_put(LED_R, 1);
-  buzzerBeep(20);
-  gpio_put(LED_R, 0);
-}
-
-void gpio_irq_handler(uint gpio, uint32_t events){
-  uint32_t current_time = to_us_since_boot(get_absolute_time()); // captura o momento do acionamento e converte para microsegundos
-  if(current_time - last_time > DEBOUNCE_TIME){
-      if (gpio == botao_A) {
-        letra ++;
-        if(letra > 26){
-          letra = 0;
-        }
-      }
-      else if(gpio == botao_B){
-        select = true;
-      }
-      else if(gpio == botao_Joystick){
-        reset_usb_boot(0, 0); //func para entrar no modo bootsel 
-      }
-      last_time = current_time; // Atualiza o tempo para o debounce
-  } // debounce
-}
+// Funcoes
+void buzzerBeep(uint duration); // Ativacao do Buzzer
+void traco(); // Representacao sonora e luminosa
+void ponto(); // Representacao sonora e luminosa
+void gpio_irq_handler(uint gpio, uint32_t events); // Interrupcoes dos botoes
 
 int main(){
-  gpio_init(botao_A);
-  gpio_set_dir(botao_A, GPIO_IN);
-  gpio_pull_up(botao_A);
-  gpio_set_irq_enabled_with_callback(botao_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+  gpio_init(BOTAO_A);
+  gpio_set_dir(BOTAO_A, GPIO_IN);
+  gpio_pull_up(BOTAO_A);
+  gpio_set_irq_enabled_with_callback(BOTAO_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
   
-  gpio_init(botao_B);
-  gpio_set_dir(botao_B, GPIO_IN);
-  gpio_pull_up(botao_B);
-  gpio_set_irq_enabled_with_callback(botao_B, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+  gpio_init(BOTAO_B);
+  gpio_set_dir(BOTAO_B, GPIO_IN);
+  gpio_pull_up(BOTAO_B);
+  gpio_set_irq_enabled_with_callback(BOTAO_B, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
 
   // Para ser utilizado o modo BOOTSEL com botão do joystick
-  gpio_init(botao_Joystick);
-  gpio_set_dir(botao_Joystick, GPIO_IN);
-  gpio_pull_up(botao_Joystick); 
-  gpio_set_irq_enabled_with_callback(botao_Joystick, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
-
-  gpio_init(LED_B);
-  gpio_set_dir(LED_B, GPIO_OUT);
-
-  gpio_init(LED_G);
-  gpio_set_dir(LED_G, GPIO_OUT);
+  gpio_init(BOTAO_JOYSTICK);
+  gpio_set_dir(BOTAO_JOYSTICK, GPIO_IN);
+  gpio_pull_up(BOTAO_JOYSTICK); 
+  gpio_set_irq_enabled_with_callback(BOTAO_JOYSTICK, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
 
   gpio_init(LED_R);
   gpio_set_dir(LED_R, GPIO_OUT);
 
 
   // Inicializando pino do buzzer
-  gpio_init(BUZZER_PIN);
-  gpio_set_dir(BUZZER_PIN, GPIO_OUT);
+  gpio_init(BUZZER);
+  gpio_set_dir(BUZZER, GPIO_OUT);
 
   // I2C Initialisation. Using it at 400Khz.
   i2c_init(I2C_PORT, 400 * 1000);
@@ -183,3 +142,45 @@ int main(){
     sleep_ms(100);
   }
 }
+
+// Função alternativa para gerar som SEM PWM (Buzzer Toggling)
+void buzzerBeep(uint duration) {
+  for (int i = 0; i < duration; i++) {
+      gpio_put(BUZZER, 1);
+      sleep_ms(5);
+      gpio_put(BUZZER, 0);
+      sleep_ms(5);
+  }
+}
+
+void traco(){
+  gpio_put(LED_R, 1); 
+  buzzerBeep(80);
+  gpio_put(LED_R, 0);
+}
+
+void ponto(){  
+  gpio_put(LED_R, 1);
+  buzzerBeep(20);
+  gpio_put(LED_R, 0);
+}
+
+void gpio_irq_handler(uint gpio, uint32_t events){
+  uint32_t current_time = to_us_since_boot(get_absolute_time()); // captura o momento do acionamento e converte para microsegundos
+  if(current_time - last_time > DEBOUNCE_TIME){
+      if (gpio == BOTAO_A) {
+        letra ++;
+        if(letra > 26){
+          letra = 0;
+        }
+      }
+      else if(gpio == BOTAO_B){
+        select = true;
+      }
+      else if(gpio == BOTAO_JOYSTICK){
+        reset_usb_boot(0, 0); //func para entrar no modo bootsel 
+      }
+      last_time = current_time; // Atualiza o tempo para o debounce
+  } // debounce
+}
+
